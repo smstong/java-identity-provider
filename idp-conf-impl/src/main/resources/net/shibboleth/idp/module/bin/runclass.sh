e0709339bc29fd12f4f35a257fa3b173bb09ca35
Rod Widdowson <rdw@steadingsoftware.com>
2024-08-10 16:38:55 +0100

    Remove AIX support from runclass.sh

--- idp-conf-impl/src/main/resources/net/shibboleth/idp/module/bin/runclass.sh
+++ idp-conf-impl/src/main/resources/net/shibboleth/idp/module/bin/runclass.sh
@@ -10,12 +10,7 @@ LOCATION=$(dirname $0)
 
 if [ -z "$JAVACMD" ] ; then 
   if [ -n "$JAVA_HOME"  ] ; then
-    if [ -x "$JAVA_HOME/jre/sh/java" ] ; then 
-      # IBM's JDK on AIX uses strange locations for the executables
-      JAVACMD=$JAVA_HOME/jre/sh/java
-    else
-      JAVACMD=$JAVA_HOME/bin/java
-    fi
+    JAVACMD=$JAVA_HOME/bin/java
   else
     JAVACMD=$(which java)
   fi
